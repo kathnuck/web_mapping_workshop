dcb1ed4d813b9ce4c1d8c3e778959e0f014ad586
kathnuck <kathnuck@users.noreply.github.com>
2016-04-25 10:43:28 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -37,3 +37,32 @@ featureLayer.on('ready', function(){
   	layer.bindPopup('Welcome to ' + layer.feature.properties.LABEL);
 	})
 })
+
+var clickHandler = function(e){
+  
+  $('#info').empty();
+  
+  var feature = e.target.feature;
+  
+  $('info').fadeIn(400, function(){
+    
+    var info = '';
+    
+    info += '<div>'
+    info +=		'<h2>' + feature.properties.LABEL + '</h2>';
+    info +=		'<p>' + feature.properties.LOCATION + '</p>'
+    info += '</div>'
+    
+    $('#info').append(info);
+    
+    })
+}
+featureLayer.on('ready'), function(){
+ 	this.eachLayer(function(layer){
+    	layer.on('click', clickHandler);
+    })
+])
+map.on('click', function(){
+  $('#info').fadeOut(200);
+  $('info').empty();
+})
