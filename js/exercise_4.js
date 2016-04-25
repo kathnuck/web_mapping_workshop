796bb0634d6ba01cefdb21d5a26d9e026cbd4b16
kathnuck <kathnuck@users.noreply.github.com>
2016-04-25 10:05:50 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -33,7 +33,7 @@ featureLayer.on('ready', function(){
 })
 
 featureLayer.on('ready', function(){
-  this.eachLayer(layer){
+  this.eachLayer(function(layer){
   	layer.bindPopup('Welcome to ' + layer.feature.properties.LABEL);
 	})
 })
